38b07a888f9112e98b7b95e3c1e22ff1a8834a80
Miguel Feo <miguel.feo@vmlyr.com>
2020-04-26 15:13:04 -0500

    Java theory finaliced

---
+++ src/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args) {
+        String test = "012345";
+        String another = test.substring(0, 1);
+        String two = test.substring(4);
+
+        System.out.println(two);
+    }
+}
