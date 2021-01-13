fcfc11b1827b9e905cdb9c4880c055b8b17faa88
Miguel Feo <feolinaresm@gmail.com>
2021-01-13 17:52:24 -0500

    Structure modified

--- src/Test.java
+++
@@ -1,9 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        String test = "012345";
-        String another = test.substring(0, 1);
-        String two = test.substring(4);
-
-        System.out.println(two);
-    }
-}
